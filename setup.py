e369c34b7a43765dac78fa783f7ce3a3d32b4425
carderm <michaelcard_GitHub@outlook.com>
2019-01-24 22:28:57 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangocms_charts',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='DjangoCMS Plugin to add and edit ChartJs charts',
     long_description=README,
-    url='https://www.michaelcarder.co.uk/',
-    author='Michael Carder',
-    author_email='contact@michaelcarder.co.uk',
+    url='https://github.com/mcldev/djangocms-charts',
+    author='Michael Carder Ltd',
+    author_email='codetoolbox@michaelcarder.co.uk',
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
