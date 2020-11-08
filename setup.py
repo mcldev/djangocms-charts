6de3d35758da39bda175406817ddbd1b0b0d5c59
carderm <michaelcard_GitHub@outlook.com>
2020-11-09 10:32:54 +1100

    Update version for type length fix.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangocms_charts',
-    version='3.0.1',
+    version='3.0.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
