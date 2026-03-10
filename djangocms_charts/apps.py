74c3011746c6e8a0aa5dbaeb606223997d137e30
carderm <mc.github@michaelcarder.co.uk>
2026-03-10 23:45:08 +1100

    Add .idea/ to .gitignore

--- djangocms_charts/apps.py
+++ djangocms_charts/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 class ChartsConfig(AppConfig):
     name = 'djangocms_charts'
     verbose_name = 'DjangoCMS Charts'
-
+    default_auto_field = 'django.db.models.AutoField'
